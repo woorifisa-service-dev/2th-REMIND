d2a4efbf1a83fd7038fa511570208c0ea5432425
{dlwhdals5059} <dlwhdals5059@gmail.com>
2023-05-04 11:32:23 +0900

    chore: substring

--- api/movieApi.js
+++ api/movieApi.js
@@ -10,13 +10,20 @@ export const movieApi = {
             .then((res) => res.json())
             .then((data) => movies = data.results)
             .catch((err) => console.error('API 에러: ', err));
-
-        const createList = movies.slice(6, 9).map((movie) => {
-
+            
+        const a = Math.floor(Math.random() * 10);
+        const createList = movies.slice(a, a+3).map((movie) => {
+          function titlelimit(title){
+            if(title.length>12){
+              return title = title.substr(0,12) + '...';
+            } else{
+              return title;
+            }
+          }
             const imageUrl = `https://image.tmdb.org/t/p/w200/${movie.poster_path}`;
             return `<li class="event-li hover:scale-[1.1] transition duration-500" data-id="${movie.id}">
                  <div class="poster shadow-[10px_1px_11px_-5px_rgba(0,0,0,0.3)]" style="background-image: url(${imageUrl});"></div>
-                 <h3 class="poster-title dark:text-white">${movie.title}</h3>
+                 <h3 class="poster-title dark:text-white">${titlelimit(movie.title)}</h3>
                 </li>`;
         });
         
@@ -57,13 +64,20 @@ export const movieApi = {
             return content
           }
         }
+        function titlelimit(title){
+          if(title.length>12){
+            return title = title.substr(0,12) + '...';
+          } else{
+            return title;
+          }
+        }
         const template = `<div class="container-rt-child" style="background-image: url(${backBgPath})">
         <div class="container-rt-blur">
           <div class="rt-poster-container">
             <div class="rt-poster" style="background-image: url(${posterBgPath})"></div>
             <div class="rt-poster-detail">
               <div class="rt-poster-detail__title">
-                <p>${movieTitle}</p>
+                <p>${titlelimit(movieTitle)}</p>
               </div>
               <div class="rt-poster-detail__description">
                 <p>
@@ -87,3 +101,4 @@ export const movieApi = {
         document.querySelector('.container-rt').innerHTML = template;
     }
 };
+
